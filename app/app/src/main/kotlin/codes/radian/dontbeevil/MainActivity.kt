01df77d507ad0fb16ed0379c8cc3447ba7e832f4
Radon Rosborough <radon@intuitiveexplanations.com>
2023-04-16 17:31:31 -0700

    Add godawful XML to construct nav bar

--- app/app/src/main/kotlin/codes/radian/dontbeevil/MainActivity.kt
+++ app/app/src/main/kotlin/codes/radian/dontbeevil/MainActivity.kt
@@ -1,11 +1,26 @@
 package codes.radian.dontbeevil
 
-import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : Activity() {
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navbar_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navbar = findViewById<BottomNavigationView>(R.id.navbar)
+
+        NavigationUI.setupWithNavController(navbar, navController)
+
+        navbar.setOnItemSelectedListener {
+            NavigationUI.onNavDestinationSelected(it, navController)
+        }
     }
 }
---
+++ app/app/src/main/kotlin/codes/radian/dontbeevil/SearchFragment.kt
@@ -0,0 +1,23 @@
+package codes.radian.dontbeevil
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
